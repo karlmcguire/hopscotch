382a69cd4a84259ff25eddf3b7286e744c13eb1f
Karl McGuire <karl@xba.io>
2019-12-07 19:01:07 -0500

    working

--- hopscotch.go
+++ hopscotch.go
@@ -1,42 +1,107 @@
 package hopscotch
 
-import "fmt"
+import (
+	"fmt"
+	"math/bits"
+)
 
 const (
 	H = 4
 )
 
-type Bucket struct {
-	Bitmap uint64
-	Key    uint64
-	Value  []byte
+type bucket struct {
+	bitmap uint64
+	key    uint64
+	value  []byte
 }
 
 type Map struct {
-	buckets []Bucket
+	buckets []bucket
 	mask    uint64
 }
 
 func New(size uint64) *Map {
 	return &Map{
-		buckets: make([]Bucket, size),
+		buckets: make([]bucket, size),
 		mask:    size - 1,
 	}
 }
 
-func (m *Map) Set(key uint64, value []byte) {
+func (m *Map) Set(key uint64, value []byte) bool {
+	index := key & m.mask
+
+	// if the key already exists
+	if m.lookup(key, index) != nil {
+		return false
+	}
+
+	for i := index; i <= m.mask; i++ {
+		bucket := &m.buckets[i]
+		if bucket.value == nil {
+			for j := uint64(1); i-index >= H; {
+				for ; j < H; j++ {
+					if m.buckets[i-j].bitmap != 0 {
+						offset := uint64(
+							bits.TrailingZeros64(m.buckets[i-j].bitmap))
+						if offset >= j {
+							continue
+						}
+						m.buckets[i].key = m.buckets[i-j+offset].key
+						m.buckets[i].value = m.buckets[i-j+offset].value
+
+						m.buckets[i-j+offset].key = 0
+						m.buckets[i-j+offset].value = nil
+
+						m.buckets[i-j].bitmap &= ^(1 << offset)
+						m.buckets[i-j].bitmap |= 1 << j
+
+						i = i - j + offset
+						break
+					}
+				}
+				if j >= H {
+					return false
+				}
+			}
+			// add the item
+			bucket.key = key
+			bucket.value = value
+			setBitmap(&bucket.bitmap, i-index)
+			return true
+		}
+	}
+	return false
 }
 
 func (m *Map) Get(key uint64) []byte {
-	return nil
+	bucket := &m.buckets[key&m.mask]
+	return bucket.value
 }
 
 func (m *Map) String() string {
 	var out string
 	for i, bucket := range m.buckets {
-		out += fmt.Sprintf(
-			"%2d: [%02d] = '%s'\n", i, bucket.Key, string(bucket.Value),
+		out += fmt.Sprintf("%2d: [%02d]-[%04b] = %s\n", i,
+			bucket.key,
+			bucket.bitmap,
+			string(bucket.value),
 		)
 	}
 	return out
+}
+
+func (m *Map) lookup(key, index uint64) *bucket {
+	for i := uint64(0); i < H && (index+i < m.mask+1); i++ {
+		bucket := &m.buckets[index+i]
+		if bucket.bitmap&(1<<i) == 1 {
+			if bucket.key == key {
+				return bucket
+			}
+		}
+	}
+	return nil
+}
+
+func setBitmap(bitmap *uint64, offset uint64) {
+	*bitmap |= 1 << offset
 }
--- hopscotch_test.go
+++ hopscotch_test.go
@@ -7,5 +7,11 @@ import (
 
 func TestMap(t *testing.T) {
 	m := New(16)
+
+	m.Set(5, []byte("five"))
+	fmt.Println(m)
+	m.Set(21, []byte("twenty one"))
+	fmt.Println(m)
+	m.Set(37, []byte("thirty seven"))
 	fmt.Println(m)
 }
